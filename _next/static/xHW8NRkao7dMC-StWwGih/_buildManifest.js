35a7b82bd48ad854004251cfed6837d8f7b749e9
Florent Zingarelli <florent@data-funnel.io>
2026-05-19 14:16:02 +0000

    fix: add CORS proxy for OpenSky API

---
+++ _next/static/xHW8NRkao7dMC-StWwGih/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/emirates-tracker//_next/:path+",
+        "destination": "/emirates-tracker/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
